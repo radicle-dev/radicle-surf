034fa066457e304b7c28dec4eae0d95d4bb9a82a
FintanH <fintan.halpenny@gmail.com>
2020-01-02 15:11:22 +0000

    Move the lib documentation to use git-platinum

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,21 @@
 //! use pretty_assertions::assert_eq;
 //!
 //! // We're going to point to this repo.
-//! let repo = GitRepository::new(".").unwrap();
+//! let repo = GitRepository::new("./data/git-platinum").expect("Failed to initialise repo");
 //!
 //! // Here we initialise a new Broswer for a the git repo.
-//! let mut browser = GitBrowser::new(&repo).unwrap();
+//! let mut browser = GitBrowser::new(&repo).expect("Failed to initialise browser");
 //!
 //! // Set the history to a particular commit
-//! browser.commit(Sha1::new("840e75edc46c84b6392b3f38e1d830547fac89a4"))
+//! browser.commit(Sha1::new("80ded66281a4de2889cc07293a8f10947c6d57fe"))
 //!        .expect("Failed to set commit");
 //!
 //! // Get the snapshot of the directory for our current
 //! // HEAD of history.
-//! let directory = browser.get_directory().unwrap();
+//! let directory = browser.get_directory().expect("Failed to get directory");
 //!
 //! // Let's get a Path to this file
-//! let this_file = Path::from_labels("src".into(), &["lib.rs".into()]);
+//! let this_file = Path::from_labels(Label::unsafe_label("src"), &[Label::unsafe_label("memory.rs")]);
 //!
 //! // And assert that we can find it!
 //! assert!(directory.find_file(&this_file).is_some());
@@ -36,26 +36,26 @@
 //! root_contents.sort();
 //!
 //! assert_eq!(root_contents, vec![
-//!     SystemType::directory(".buildkite".into()),
-//!     SystemType::directory(".docker".into()),
-//!     SystemType::file(".gitignore".into()),
-//!     SystemType::file(".gitmodules".into()),
-//!     SystemType::file("Cargo.toml".into()),
-//!     SystemType::file("README.md".into()),
-//!     SystemType::directory("docs".into()),
-//!     SystemType::directory("examples".into()),
-//!     SystemType::directory("src".into()),
+//!     SystemType::file(Label::unsafe_label(".i-am-well-hidden")),
+//!     SystemType::file(Label::unsafe_label(".i-too-am-hidden")),
+//!     SystemType::file(Label::unsafe_label("README.md")),
+//!     SystemType::directory(Label::unsafe_label("bin")),
+//!     SystemType::directory(Label::unsafe_label("src")),
+//!     SystemType::directory(Label::unsafe_label("text")),
+//!     SystemType::directory(Label::unsafe_label("this")),
 //! ]);
 //!
-//! let src = directory.find_directory(&Path::new("src".into())).unwrap();
+//! let src = directory.find_directory(
+//!     &Path::new(Label::unsafe_label("src"))
+//! ).expect("Failed to find src");
+//!
 //! let mut src_contents = src.list_directory();
 //! src_contents.sort();
 //!
 //! assert_eq!(src_contents, vec![
-//!     SystemType::directory("diff".into()),
-//!     SystemType::directory("file_system".into()),
-//!     SystemType::file("lib.rs".into()),
-//!     SystemType::directory("vcs".into()),
+//!     SystemType::file(Label::unsafe_label("Eval.hs")),
+//!     SystemType::file(Label::unsafe_label("Folder.svelte")),
+//!     SystemType::file(Label::unsafe_label("memory.rs")),
 //! ]);
 //! ```
 pub mod diff;
