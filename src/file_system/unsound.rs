3a1fb706b3e760edf17f13d1838266c3659d86fd
FintanH <fintan.halpenny@gmail.com>
2020-01-02 17:46:27 +0000

    Add unsound module for try_from calls that will fail with expect

---
+++ src/file_system/unsound.rs
@@ -0,0 +1,27 @@
+pub mod path {
+    use crate::file_system::Path;
+    use std::convert::TryFrom;
+
+    /// **NB**: Use with caution!
+    ///
+    /// Calls `try_from` on the input and expects it to not fail.
+    ///
+    /// Used for testing and playground purposes.
+    pub fn new(path: &str) -> Path {
+        Path::try_from(path).expect("unsafe_path: Failed to parse path")
+    }
+}
+
+pub mod label {
+    use crate::file_system::Label;
+    use std::convert::TryFrom;
+
+    /// **NB**: Use with caution!
+    ///
+    /// Calls `try_from` on the input and expects it to not fail.
+    ///
+    /// Used for testing and playground purposes.
+    pub fn new(path: &str) -> Label {
+        Label::try_from(path).expect("unsafe_path: Failed to parse label")
+    }
+}
