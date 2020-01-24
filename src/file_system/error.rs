cb24a93e55d3afbedaec95fa4f5de6cd83f7bf62
FintanH <fintan.halpenny@gmail.com>
2020-01-24 22:06:53 +0000

    Elaboration on file system error module docs and linking clean up

--- src/file_system/error.rs
+++ src/file_system/error.rs
@@ -1,4 +1,7 @@
 //! Errors that can occur within the file system logic.
+//!
+//! These errors occur due to [`Label`] and [`Path`] parsing when using their respective `TryFrom`
+//! instances.
 
 pub(crate) const EMPTY_PATH: Error = Error::Path(Path::Empty);
 
@@ -9,9 +12,9 @@ pub(crate) const CONTAINS_SLASH: Error = Error::Label(Label::ContainsSlash);
 /// Error type for all file system errors that can occur.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error {
-    /// A `Label` specific error for parsing a `Label`.
+    /// A `Label` specific error for parsing a [`Label`].
     Label(Label),
-    /// A `Path` specific error for parsing a `Path`.
+    /// A `Path` specific error for parsing a [`Path`].
     Path(Path),
 }
 
@@ -27,20 +30,20 @@ impl From<Path> for Error {
     }
 }
 
-/// Parse errors for when parsing a string to a [`Path`](struct.Path.html).
+/// Parse errors for when parsing a string to a [`Path`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Path {
-    /// An error signifying that a `Path` is empty.
+    /// An error signifying that a [`Path`] is empty.
     Empty,
 }
 
-/// Parse errors for when parsing a string to a [`Label`](struct.Label.html).
+/// Parse errors for when parsing a string to a [`Label`].
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Label {
-    /// An error signifying that a `Label` is contains invalid UTF-8.
+    /// An error signifying that a [`Label`] is contains invalid UTF-8.
     InvalidUTF8,
-    /// An error signifying that a `Label` contains a `/`.
+    /// An error signifying that a [`Label`] contains a `/`.
     ContainsSlash,
-    /// An error signifying that a `Label` is empty.
+    /// An error signifying that a [`Label`] is empty.
     Empty,
 }
