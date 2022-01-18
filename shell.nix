fce8c8b0f50ecaef0f80088497241ea3f6deeae8
Fintan Halpenny <fintan.halpenny@gmail.com>
2022-01-18 15:52:09 +0000

    nix: update nix to allow for rustc stable release
    Signed-off-by: Fintan Halpenny <fintan.halpenny@gmail.com>

--- shell.nix
+++ shell.nix
@@ -4,21 +4,23 @@
   }
 }:
 let
-    rustToolChain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain);
-    rust = rustToolChain.override {
-        extensions = [ "rust-src" "rust-analysis" ];
-    };
+  stable = pkgs.rust-bin.stable.latest.default;
+  rust = stable.override {
+    extensions = [ "rust-src" "rust-analysis" ];
+  };
 in
 with pkgs;
 mkShell {
     name = "radicle-surf";
     buildInputs = [
+        cargo-deny
         cargo-expand
         cargo-watch
         # gnuplot for benchmark purposes
         gnuplot
         pkgconfig
         openssl
+        pkgs.rust-bin.nightly."2021-12-02".rustfmt
         ripgrep
         rust
         zlib
